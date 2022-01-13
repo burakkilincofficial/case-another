e8e06bbd552efbd73d4937beb61f6bfcd7f3603f
Burak KILINC <burakkilincofficial@gmail.com>
2022-01-13 23:14:35 +0300

    dd

---
+++ src/com/bodesere/Main.java
@@ -0,0 +1,12 @@
+package com.bodesere;
+
+/**
+ * TODO Describe this class.
+ * @since 2021-10-09
+ * @author burak kilinc
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
